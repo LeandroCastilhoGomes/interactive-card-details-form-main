c5b15b87b3e2d470f9d90e9b780855720c5e9c27
LeandroCastilhoGomes <119023278+LeandroCastilhoGomes@users.noreply.github.com>
2023-03-11 23:41:52 -0300

    declarando as variavéis no script

--- script/script.js
+++ script/script.js
@@ -1,5 +1,14 @@
-var nome = document.getElementById('nome').innerHTML
-var numero = document.getElementById('numero').innerHTML
-var mes = document.getElementById('mes')
-var ano = document.getElementById('ano')
-var cvc = document.getElementById('codigo')
+var form = document.getElementById('form')
+var concluido = getElementById('concluido')
+
+var nomeins = document.getElementById('nome')
+var numeroins = document.getElementById('numero')
+var mesins = document.getElementById('mes')
+var anoins = document.getElementById('ano')
+var cvcins = document.getElementById('codigo')
+
+var nomecard = document.getElementById('nome-card')
+var numcard = document.getElementById('numcard')
+var mescard = document.getElementById('card-mes')
+var anocard = document.getElementById('card-ano')
+var cvccard = document.getElementById('cod-seg')
