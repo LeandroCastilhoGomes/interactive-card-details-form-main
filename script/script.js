22458e0e225cc69bce8967b4639499ca9662621c
LeandroCastilhoGomes <119023278+LeandroCastilhoGomes@users.noreply.github.com>
2023-03-11 23:22:23 -0300

    ajustes nas divs mes e ano

--- script/script.js
+++ script/script.js
@@ -1,2 +1,5 @@
 var nome = document.getElementById('nome').innerHTML
 var numero = document.getElementById('numero').innerHTML
+var mes = document.getElementById('mes')
+var ano = document.getElementById('ano')
+var cvc = document.getElementById('codigo')
